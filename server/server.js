d3126210b1dd7e8a47014550392c41b860939d83
shubhFreelance <shubham58ch@gmail.com>
2025-03-23 14:15:11 +0530

    cron job added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cron from 'node-cron';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
 import betRoutes from './routes/bet.js';
@@ -6,7 +8,8 @@ import sessionRoutes from './routes/session.js';
 import agentRoutes from './routes/agent.js';
 import reportRoutes from './routes/report.js';
 import resultRoutes from './routes/result.js';
-import dotenv from 'dotenv';
+import { calculateDailyCollections, calculateSuperadminDailyCollection } from './controllers/agentController.js';
+
 
 dotenv.config();
 const app = express();
@@ -30,3 +33,9 @@ app.use('/api/result', resultRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+cron.schedule('59 23 * * *', async () => {
+  console.log('Calculating daily collections...');
+  await calculateDailyCollections(); // Calculate daily collections for agents
+  await calculateSuperadminDailyCollection(); // Calculate daily collection for superadmin
+});cd
